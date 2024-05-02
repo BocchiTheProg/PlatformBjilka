5fac790dbd07bfb46c10d5535d330190a35522b2
BocchiTheProg <bocchitheprog@gmail.com>
2024-05-02 04:13:50 +0300

    Add: Ukrainian localization for main texts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_admin_user!
+  around_action :switch_locale
+
+  def switch_locale(&)
+    params_locale = I18n.locale_available?(params[:locale]) && params[:locale]
+    locale = params_locale || locale_from_header || I18n.default_locale
+    I18n.with_locale(locale, &)
+  end
+
+  def locale_from_header
+    locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    I18n.locale_available?(locale) && locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
